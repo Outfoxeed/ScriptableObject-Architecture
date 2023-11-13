f3cfab3a556be30ec5a17be743d4d15bd284dd4e
Outfoxeed <letourneaumorigan@gmail.com>
2023-11-14 00:17:24 +0100

    [ADD] CusotmEditor for SoArchObject and Variables

---
+++ Core/Editor/ScriptableObjectArchitectureObjectEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using ScriptableObjectArchitecture.Base;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ScriptableObjectArchitecture.Editor
+{
+    [CustomEditor(typeof(ScriptableObjectArchitectureObject), true)]
+    public class ScriptableObjectArchitectureObjectEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            DrawDebugButton();
+            DrawPropertiesExcluding(serializedObject, new []{"m_Script", "<DebugMode>k__BackingField"});
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawDebugButton()
+        {
+            if (target is not ScriptableObjectArchitectureObject soArchObject)
+            {
+                return;
+            }
+
+            FieldInfo fieldInfo = typeof(ScriptableObjectArchitectureObject).GetField("<DebugMode>k__BackingField",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fieldInfo is null)
+            {
+                GUILayout.Label("Cannot find the DebugMode field info", new GUIStyle(GUI.skin.button){normal = {textColor = Color.red}});
+                return;
+            }
+
+            bool debugMode = (bool)fieldInfo.GetValue(soArchObject);
+            
+            GUIStyle style = debugMode
+                ? new GUIStyle(GUI.skin.button){normal = {textColor = Color.red}, hover = {textColor = Color.red}}
+                : GUI.skin.button;
+            string buttonLabel = "Debug Mode " + (debugMode ? "ON" : "OFF");
+            if (GUILayout.Button(buttonLabel, style))
+            {
+                fieldInfo.SetValue(soArchObject, !debugMode);
+            }
+        }
+    }
+}
---
+++ Core/Editor/Variables/VariableEditor.cs
@@ -0,0 +1,28 @@
+﻿using ScriptableObjectArchitecture.Variables;
+using UnityEditor;
+using UnityEngine;
+using Logger = ScriptableObjectArchitecture.Utils.Logger;
+
+namespace ScriptableObjectArchitecture.Editor.Editor.Variables
+{
+    [CustomEditor(typeof(Variable<>), true)]
+    public class VariableEditor : ScriptableObjectArchitectureObjectEditor
+    {
+        private const string PropertyName = "_property";
+        
+        public override void OnInspectorGUI()
+        {
+            SerializedProperty spProperty = serializedObject.FindProperty(PropertyName);
+            if (spProperty is null)
+            {
+                string errorMsg = $"Cannot find SerializedProperty of name '{PropertyName}'";
+                GUILayout.Label(errorMsg);
+                Logger.Instance?.LogError(errorMsg);
+            }
+
+            spProperty.isExpanded = true;
+            
+            base.OnInspectorGUI();
+        }
+    }
+}
